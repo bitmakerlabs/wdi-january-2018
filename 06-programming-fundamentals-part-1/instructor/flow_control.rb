ab11ccad778df45f51a800c93da4ca31a3f932f6
Daniel Moniz <daniel.moniz@gmail.com>
2018-01-29 12:37:52 -0500

    Add intro to programming code.

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -1,3 +1,4 @@
+
 # if statements
 
 # if (some condition is true)
@@ -5,8 +6,35 @@
 # (optionally) otherwise
 #   then do something else
 
+apples = 2
+# apples = apples - 8
+
+puts apples
+
+if apples < 3
+  puts "Need to buy apples"
+elsif apples < 6
+  puts "Heads up, you're running out"
+else
+  puts "Go ahead, you're good!"
+end
 
 
+dark_outside = true
+
+unless dark_outside
+  puts "I'm going to play in the park."
+end
+# NO OUTPUT
+
+puts "Apples for sale!" if apples > 0
+# IS THE SAME AS ...
+if apples > 0
+  puts "Apples for sale!"
+end
+
+# ternary statement
+puts apples > 0 ? "Apples for sale!" : "Sorry, no apples today."
 
 
 # Looping
@@ -18,3 +46,17 @@
 
 
 # Iteration
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
