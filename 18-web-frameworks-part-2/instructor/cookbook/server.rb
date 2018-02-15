052ff183e4957ca8e1cf9022ececf7a30b516550
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-02-15 10:35:09 -0500

    [18-web-frameworks-part-2] Add a layout

--- 18-web-frameworks-part-2/instructor/cookbook/server.rb
+++ 18-web-frameworks-part-2/instructor/cookbook/server.rb
@@ -5,17 +5,21 @@ get '/' do
 end
 
 get '/about' do
+  @title = 'About Us'
   erb :about
 end
 
 get '/contact' do
+  @title = 'Contact Us'
   erb :contact
 end
 
 get '/blog' do
+  @title = 'Blog'
   erb :blog
 end
 
 get '/recipes' do
+  @title = 'Recipes'
   erb :recipes
 end
