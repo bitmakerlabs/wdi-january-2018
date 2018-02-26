334689737fbfb54524b352601c420e709f0897bf
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-02-26 12:05:46 -0500

    [25-migrations-and-validations] Add validations on Track

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,6 +3,25 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
+  # t.string "name", null: false
+  # t.string "composer"
+  # t.integer "milliseconds", null: false
+  # t.integer "bytes"
+  # t.decimal "unit_price", precision: 10, scale: 2
+
+  validates :name, :composer, :milliseconds, :bytes, :unit_price, presence: true
+
+  validates :milliseconds, :bytes, numericality: {
+                                                    only_integer: true,
+                                                    greater_than: 0
+                                                 }
+
+  validates :unit_price, numericality: {
+                                         greater_than_or_equal_to: 0.0
+                                       }
+
+  validate :name_must_be_capitalized
+
   scope :by_name, -> { order(name: :asc) }
   scope :short,   -> { shorter_than(SHORT) }
   scope :long,    -> { longer_than_or_equal_to(LONG) }
@@ -33,5 +52,19 @@ class Track < ApplicationRecord
   scope :longer_than_or_equal_to, -> (milliseconds) {
     where('milliseconds >= ?', milliseconds) if milliseconds && milliseconds > 0
   }
+
+private
+
+  def name_must_be_capitalized
+    # Step 1: Make sure there is a name!
+    return unless name.present?
+
+    # Step 2: Check that the first char is an uppercase letter
+    first_char = name[0]
+    first_char_is_not_upcased = (first_char != first_char.upcase)
+
+    # Step 3: If first char is not uppercase, add an error
+    errors.add(:name, 'must be capitalized!') if first_char_is_not_upcased
+  end
 
 end
