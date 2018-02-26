808192d46d865afdef47cc0e5140421e57b632ce
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-02-26 10:31:05 -0500

    [25-migrations-and-validations] First set of class methods and scopes on Track

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -1,2 +1,23 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG  = 360000
+
+  scope :by_name, -> { order(name: :asc) }
+  scope :short,   -> { shorter_than(SHORT) }
+  scope :long,    -> { longer_than_or_equal_to(LONG) }
+  scope :medium,  -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
+
+  def self.starts_with(char)
+    where('name ILIKE ?', "#{ char }%")
+  end
+
+  def self.shorter_than(milliseconds)
+    where('milliseconds < ?', milliseconds)
+  end
+
+  def self.longer_than_or_equal_to(milliseconds)
+    where('milliseconds >= ?', milliseconds)
+  end
+
 end
