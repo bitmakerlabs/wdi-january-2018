d380f22ae92bd15748075dc80de6c4162d4e41f4
Sean Lerner <sean@smallcity.ca>
2018-02-27 10:02:05 -0500

    simponson data for associations lesson

---
+++ 26-associations/db/data/ruby/actors.rb
@@ -0,0 +1,26 @@
+actors = [
+
+  { first_name: 'Dan',      last_name: 'Castellaneta',   type: 'Main',           characters: ["Homer Simpson", "Abe Simpson", "Krusty the Clown", "Barney Gumble", "Groundskeeper Willie", "Mayor Quimby", "Hans Moleman", "Sideshow Mel", "Squeaky Voiced Teen", "Gil", "Rich Texan"] },
+  { first_name: 'Nancy',    last_name: 'Cartwright',     type: 'Main',           characters: ["Bart Simpson", "Nelson Muntz", "Ralph Wiggum", "Kearny"] },
+  { first_name: 'Julie',    last_name: 'Kavner',         type: 'Main',           characters: ["Marge Simpson", "Patty Bouvier", "Selma Bouvier"] },
+  { first_name: 'Yeardley', last_name: 'Smith',          type: 'Main',           characters: ["Lisa Simpson"] },
+  { first_name: 'Hank',     last_name: 'Azaria',         type: 'Main',           characters: ["Moe Szyslak", "Chief Wiggum", "Apu Nahasapeemapetilon", "Carl", "Comic Book Guy", "Lou", "Prof. Frink", "Cletus", "Superintendent Chalmers", "Snake", "Sea Captain", "Kirk Van Houten", "Wiseguy", "Bumblebee Man", "Disco Stu", "Dr. Nick"] },
+  { first_name: 'Harry',    last_name: 'Shearer',        type: 'Main',           characters: ["Mr. Burns", "Waylon Smithers", "Ned Flanders", "Seymour Skinner", "Lenny", "Kent Brockman", "Reverend Lovejoy", "Dr. Hibbert", "Otto", "Jasper"] },
+  { first_name: 'Pamela',   last_name: 'Hayden',         type: 'Secondary',      characters: ["Milhouse Van Houten", "Jimbo", "Rod Flanders", "Todd Flanders"] },
+  { first_name: 'Tress',    last_name: 'MacNeille',      type: 'Secondary',      characters: ["Agnes Skinner", "Crazy Cat Lady", "Lindsey Naegle", "Manjula Nahasapeemapetilon"] },
+  { first_name: 'Maggie',   last_name: 'Roswell',        type: 'Secondary',      characters: ["Maude Flanders", "Helen Lovejoy", "Ms. Hoover", "Luann Van Houten"] },
+  { first_name: 'Russi',    last_name: 'Taylor',         type: 'Secondary',      characters: ["Martin Prince", "Sherri Uter", "Terry Uter"] },
+  { first_name: 'Marcia',   last_name: 'Wallace',        type: 'Guest',          characters: ["Edna Krabappel"] },
+  { first_name: 'Phil',     last_name: 'Hartman',        type: 'Guest',          characters: ["Lionel Hutz", "Troy McClure"] },
+  { first_name: 'Joe',      last_name: 'Mantegna',       type: 'Guest',          characters: ["Fat Tony"] },
+  { first_name: 'Frank',    last_name: 'Welker',         type: 'Guest',          characters: ["Santa's Little Helper"] },
+  { first_name: 'Kelsey',   last_name: 'Grammer',        type: 'Guest',          characters: ["Sideshow Bob"] },
+  { first_name: 'Jon',      last_name: 'Lovitz',         type: 'Guest',          characters: ["Artie Ziff"] },
+  { first_name: 'Albert',   last_name: 'Brooks',         type: 'Guest',          characters: ["Hank Scorpio"] },
+  { first_name: 'Glenn',    last_name: 'Close',          type: 'Guest',          characters: ["Mona Simpson"] },
+  { first_name: 'Stephen',  last_name: 'Hawking',        type: 'Guest',          characters: ["Stephen Hawking"] }
+
+]
+
+
+
---
+++ 26-associations/db/data/ruby/quotes.rb
@@ -0,0 +1,111 @@
+
+quotes = [
+  {
+    quote: "There's only one thing to do at a moment like this: strut!",
+    characters: "Bart Simpson",
+    episode: "Bart's Girlfriend",
+    air_date: "November 6, 1994"
+    },
+  {
+    quote: "Your ideas are intriguing to me, and I wish to subscribe to your newsletter.",
+    characters: "Homer Simpson",
+    episode: "Mountain of Madness",
+    air_date: "February 2, 1997"
+  },
+  {
+    quote: "Everything's coming up Milhouse.",
+    characters: "Milhouse Van Houten",
+    episode: "Mom and Pop Art",
+    air_date: "April 11, 1999"
+  },
+  {
+    quote: "I used to be with it, but then they changed what ‘it' was, and now what I'm with isn't it. And what's ‘it' seems weird and scary to me.",
+    characters: "Abe Simpson",
+    episode: "Homerpalooza",
+    air_date: "May 19, 1996"
+  },
+  {
+    quote: "I, for one, welcome our new insect overlords.",
+    characters: "Kent Brockman",
+    episode: "Deep Space Homer",
+    air_date: "February 24, 1994"
+  },
+  {
+    quote: "It takes two to lie: one to lie and one to listen.",
+    characters: "Homer Simpson",
+    episode: "Colonel Homer",
+    air_date: "March 26, 1992"
+  },
+  {
+    quote: "I would kill everyone in this room for a drop of sweet beer.",
+    characters: "Homer Simpson",
+    episode: "Duffless",
+    air_date: "February 18, 1993"
+  },
+  {
+    quote: "They taste like burning.",
+    characters: "Ralph Wiggum",
+    episode: "Das Bus",
+    air_date: "February 15, 1998"
+  },
+  {
+    quote: "Marriage is like a coffin and each kid is another nail.",
+    characters: "Homer Simpson",
+    episode: "How I Spent My Strummer Vacation",
+    air_date: "November 10, 2002"
+  },
+  {
+    quote: "I've said it before and I'll say it again: democracy simply doesn't work.",
+    characters: "Kent Brockman",
+    episode: "Bart's Comet",
+    air_date: "February 5, 1995"
+  },
+  {
+    quote: "What's the point of going out? We're just gonna wind up back here anyway.",
+    characters: "Homer Simpson",
+    episode: "Scenes from the Class Struggle in Springfield",
+    air_date: "February 4, 1996"
+  },
+  {
+    quote: "I didn't think it was physically possible, but this both sucks and blows.",
+    characters: "Bart Simpson",
+    episode: "Marge Simpson in: 'Screaming Yellow Honkers'",
+    air_date: "February 21, 1999"
+  },
+  {
+    quote: "Loneliness and cheeseburgers are a dangerous mix.",
+    characters: "Comic Book Guy",
+    episode: "My Sister, My Sitter",
+    air_date: "March 2, 1997"
+  },
+  {
+    quote: "You tried your best and you failed miserably. The lesson is: Never try.",
+    characters: "Homer Simpson",
+    episode: "Burns' Heir",
+    air_date: "April 14, 1994"
+  },
+  {
+    quote: "There's a 4:30 in the morning now?",
+    characters: "Bart Simpson",
+    episode: "Bart's Comet",
+    air_date: "February 5, 1995"
+  },
+  {
+    quote: "Don't blame me, I voted for Kodos.",
+    characters: "Homer Simpson",
+    episode: "Treehouse of Horror VII",
+    air_date: "October 27, 1996"
+  },
+  {
+    quote: "I can't promise I'll try, but I'll try to try.",
+    characters: "Bart Simpson",
+    episode: "The Canine Mutiny",
+    air_date: "April 13, 1997"
+  },
+  {
+    quote: "My cat's breath smells like cat food.",
+    characters: "Ralph Wiggum",
+    episode: "Lisa's Rival",
+    air_date: "September 11, 1994"
+  }
+]
