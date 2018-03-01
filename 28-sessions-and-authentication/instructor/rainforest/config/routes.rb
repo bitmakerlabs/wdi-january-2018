9872e6d9a978afdc4ab045cbb57a3067c33a3828
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-03-01 11:41:40 -0500

    [28-sessions-and-authentication] Remove useless session views, change session routes to Restful

--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   resources :products, only: :index
 
   resource :users, only: %i(new create)
+
+  resource :sessions, only: %i(new create destroy)
 
   root 'products#index'
 
