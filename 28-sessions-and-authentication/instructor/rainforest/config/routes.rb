9b3a712e4977f0be6dd923367662e671f6f568a7
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-03-01 11:39:06 -0500

    [28-sessions-and-authentication] Genreate Sessions controller with: bin/rails generate controller Sessions new create destroy --no-test-framework --no-helper --no-assets

--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :products, only: :index
 
   resource :users, only: %i(new create)
