b044f57056f15643eeaa2d6f48c148a749e70c58
NatalieBlack <natalievictoriablack@gmail.com>
2018-03-09 12:56:27 -0500

    More in class code for 30-how-to-test

--- 30-how-to-test/seatyourself/app/models/restaurant.rb
+++ 30-how-to-test/seatyourself/app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ActiveRecord::Base
 
   validates :name, :opening_hour, :closing_hour, presence: true
+  validates :opening_hour, :closing_hour, numericality: { only_integer: true }
 
   belongs_to :category
   has_many :reservations
