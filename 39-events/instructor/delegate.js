2a37ae529bbb4187631920bb718170a788577150
betty <makewithbetty@gmail.com>
2018-03-26 11:27:27 -0400

    Add js events in-class code

--- 39-events/instructor/delegate.js
+++ 39-events/instructor/delegate.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Function scope
 
+  // var num = 10;
+  // var num2 = 50;
+  // var num100 = 42;
+
   var count = 0,
       colors = ['tomato', 'azure', 'green', 'orange', 'purple', 'chartreuse', 'gold', 'fuschia'];
 
   var container = document.querySelector('#container');
 
   var boxMaker = document.querySelector('#box-maker');
+
+  // Delegated Event Handler
+  // Bind event to a parent element
+  container.addEventListener('click', function(event) {
+    if (event.target.classList.contains('circle')) {
+      event.target.remove();
+    }
+  });
 
   boxMaker.addEventListener('click', function() {
       var x = Math.ceil(Math.random() * 400),
@@ -23,8 +35,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
       circle.innerHTML = count++;
 
+      // Can use `this` or `event`
+      // circle.addEventListener('click', function(event) {
+      //  this.remove();
+      //  event.target.remove();
+      // });
+
       container.append(circle);
 
       console.log('link was clicked');
   });
+
+  // DID NOT WORK - circles are not rendered on page load, they're added dynamically
+  // var circles = document.querySelectorAll('.circle');
+  //
+  // circles.forEach(function(circle) {
+  //   circle.addEventListener('click', function(e) {
+  //     e.target.remove();
+  //     // this.remove();
+  //   });
+  // });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Solutions:
+// 1. Select all `.circle` with `querySelectorAll`
+//    Iterate through them to `addEventListener`
+
+// 2. Add listener each time a circle is made
+//    Can use `this` or `target` property
+
+// 3. Delegated Event Handler
+//    bind event to a parent element (`.container`)
+//    Check if the e contains the desired class
+//    Remove ele
--- 39-events/instructor/script.js
+++ 39-events/instructor/script.js
@@ -1,1 +1,58 @@
 // Let's add our first event handlers to this simple document
+document.addEventListener('DOMContentLoaded', function() {
+
+  // 1. Select the `click-bait` element
+  var clickBait = document.querySelector('#click-bait');
+
+  // 2. Add an event listener to clickBait and ...
+  //    implement the callback with an anonymous function
+  clickBait.addEventListener('click', function(event) {
+    window.alert("I've been clicked");
+    console.log(event);
+    event.preventDefault();
+  });
+
+
+  // 3. Select the `second-level` element
+  var secondLevel = document.querySelector('#second-level');
+
+  // 4. Add an event listener to secondLevel and ...
+  //    implement the callback with a function declaration
+
+
+  secondLevel.addEventListener('click', secondLevelEventHandler);
+
+  function secondLevelEventHandler(poop) {
+    window.alert("Second level is clicked");
+    poop.stopPropagation();
+  }
+
+
+  // 5. Select the `first-level` element
+  var firstLevel = document.querySelector('#first-level');
+
+  // 6. Add an event listener to firstLevel and ...
+  //    implement the callback with a function definition
+  var firstLevelEventHandler = function() {
+    window.alert("First level is clicked!");
+  };
+
+  firstLevel.addEventListener('click', firstLevelEventHandler);
+
+
+  // 7. Check out the `event` object:
+  // => Google MDN addEventListener
+  // => https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+  // QQ: Our default behaviour of the link routes us to Google, can we stop the default behaviour?
+  // (Check out preventDefault)
+
+  // QQ: What default behaviours will you want to prevent typically? There are mainly 2:
+  // (1. links ... 2. f o r m s!!! (ryhmes with torms lol) )
+
+  // QQ: Can we stop the event from bubbling up the DOM?
+  // => https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation
+
+  // QQ: Does order of including our JavaScript matter?
+  // DOMContentLoaded => https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+});
